3034c16c94d43e1d3349ec7e647cb7b811d9bae6
Kay Robbins <Kay.Robbins@utsa.edu>
2015-01-02 15:14:59 -0600

    Fixed setdiff in robust reference for convergence

--- StandardLevel2/runStandardLevel2NCTU.m
+++ StandardLevel2/runStandardLevel2NCTU.m
@@ -5,7 +5,7 @@
 
 ess1Path = 'E:\\CTAData\\01. NCTU lane-keeping task';
 ess1File = 'E:\\CTAData\\01. NCTU lane-keeping task\\study_description.xml';
-ess2Dir = 'N:\\ARLAnalysis\\NCTU\\Level2D';
+ess2Dir = 'N:\\ARLAnalysis\\NCTU\\Level2G';
 
 %% Validate level 1
 % obj1 = level1Study(ess1File);
