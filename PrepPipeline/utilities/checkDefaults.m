52b2082585e9d08152b838794308b773c37e1078
cassidymatousek <cassidy.matousek@gmail.com>
2015-06-02 14:00:40 -0500

    check defaults field names
    Added a variable for the field names of the errors to be returned.

--- PrepPipeline/utilities/checkDefaults.m
+++ PrepPipeline/utilities/checkDefaults.m
@@ -1,6 +1,7 @@
-function [structOut, errors] = checkDefaults(structIn, structOut, defaults)
+function [structOut, errors, fNamesErrors] = checkDefaults(structIn, structOut, defaults)
 % Check structIn input parameters against defaults put in structOut
 errors = cell(0);
+fNamesErrors=cell(0);
 fNames = fieldnames(defaults);
 for k = 1:length(fNames)
     try
@@ -11,5 +12,6 @@ for k = 1:length(fNames)
        structOut.(fNames{k}) = nextValue;
     catch mex
         errors{end+1} = [fNames{k} ' invalid: ' mex.message]; %#ok<AGROW>
+        fNamesErrors{end+1}=fNames{k}; %#ok<AGROW>
     end
 end
