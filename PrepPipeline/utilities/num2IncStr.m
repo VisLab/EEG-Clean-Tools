f4843253aaafd5cbafe216c87344a28f2fb7b1a5
jcockfie <jeremy.cockfield@gmail.com>
2015-08-20 15:02:25 -0500

    implemented and refactored num2IncStr

--- PrepPipeline/utilities/num2IncStr.m
+++ PrepPipeline/utilities/num2IncStr.m
@@ -1,21 +1,40 @@
 function str = num2IncStr(num)
-        str = num2str(num(1));
-        incrementStart = true;
-        for a = 2:length(num)
-            if num(a) - num(a-1) == 1
-                if incrementStart
-                    str = [str ':']; %#ok<AGROW>
-                    incrementStart = false;
-                end
-            else
-                str = [str ',' num2str(num(a))];   %#ok<AGROW>
-                incrementStart = true;
-            end
-        end
-        if ~incrementStart
-           str = [str num2str(num(length(num)))]; 
-        end
-        
-
+% Converts a number to string with colon operators
+str = num2str(num(1));
+incrementStart = true;
+for a = 2:length(num)
+    if num(a) - num(a-1) == 1
+        incrementStart = handleSequence(incrementStart);
+    else
+        incrementStart = handleNonSequence(num(a-1), num(a), ...
+            incrementStart);
+    end
 end
+handleLastIndex(num(length(num)), incrementStart);
 
+    function incrementStart = handleNonSequence(previous, current, ...
+            incrementStart)
+        % Handles a number that is not in a sequence
+        if ~incrementStart
+            str = [str num2str(previous)];
+        end
+        str = [str ',' num2str(current)];
+        incrementStart = true;
+    end % handleNonSequence
+
+    function handleLastIndex(last, incrementStart)
+        % Handles the last number that is in a sequence or not
+        if ~incrementStart
+            str = [str num2str(last)];
+        end
+    end % handleLastIndex
+
+    function incrementStart = handleSequence(incrementStart)
+        % Handles a number that is the start of a sequence
+        if incrementStart
+            str = [str ':'];
+            incrementStart = false;
+        end
+    end % handleSequence
+
+end % num2IncStr
